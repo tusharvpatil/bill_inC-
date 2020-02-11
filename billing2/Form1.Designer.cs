1ccee7891b4c931412b94a0bcfa2f8eb8355b0ee
tusharvpatil <43710321+tusharvpatil@users.noreply.github.com>
2020-02-11 15:03:16 +0530

    Add files via upload

---
+++ billing2/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace billing2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.tot = new System.Windows.Forms.TextBox();
+            this.qun = new System.Windows.Forms.TextBox();
+            this.price = new System.Windows.Forms.TextBox();
+            this.amt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.bill = new System.Windows.Forms.TextBox();
+            this.tt = new System.Windows.Forms.TextBox();
+            this.tp = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D"});
+            this.comboBox1.Location = new System.Drawing.Point(33, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.comboBox2.Items.AddRange(new object[] {
+            "5%",
+            "5.5%",
+            "6%",
+            "6.5%",
+            "7%"});
+            this.comboBox2.Location = new System.Drawing.Point(568, 30);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(55, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // tot
+            // 
+            this.tot.Location = new System.Drawing.Point(653, 31);
+            this.tot.Name = "tot";
+            this.tot.ReadOnly = true;
+            this.tot.Size = new System.Drawing.Size(100, 20);
+            this.tot.TabIndex = 2;
+            this.tot.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // qun
+            // 
+            this.qun.Location = new System.Drawing.Point(302, 33);
+            this.qun.Name = "qun";
+            this.qun.Size = new System.Drawing.Size(100, 20);
+            this.qun.TabIndex = 3;
+            this.qun.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // price
+            // 
+            this.price.Location = new System.Drawing.Point(179, 33);
+            this.price.Name = "price";
+            this.price.ReadOnly = true;
+            this.price.Size = new System.Drawing.Size(100, 20);
+            this.price.TabIndex = 4;
+            this.price.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // amt
+            // 
+            this.amt.Location = new System.Drawing.Point(440, 32);
+            this.amt.Name = "amt";
+            this.amt.ReadOnly = true;
+            this.amt.Size = new System.Drawing.Size(100, 20);
+            this.amt.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Item";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(207, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(323, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "quantity";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(461, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "amount";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(575, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "tax";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(682, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "total";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(650, 343);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Total Tax";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(505, 343);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Total Price";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Item\tprice\tquntity\tamount\ttax\ttotal",
+            "---------------------------------------------------------------------------------" +
+                "----------------"});
+            this.listBox1.Location = new System.Drawing.Point(193, 126);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(380, 164);
+            this.listBox1.TabIndex = 14;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // bill
+            // 
+            this.bill.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bill.Location = new System.Drawing.Point(568, 418);
+            this.bill.Name = "bill";
+            this.bill.Size = new System.Drawing.Size(100, 26);
+            this.bill.TabIndex = 15;
+            // 
+            // tt
+            // 
+            this.tt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tt.Location = new System.Drawing.Point(642, 362);
+            this.tt.Name = "tt";
+            this.tt.Size = new System.Drawing.Size(100, 22);
+            this.tt.TabIndex = 16;
+            this.tt.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // tp
+            // 
+            this.tp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tp.Location = new System.Drawing.Point(499, 362);
+            this.tp.Name = "tp";
+            this.tp.Size = new System.Drawing.Size(100, 22);
+            this.tp.TabIndex = 17;
+            this.tp.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(476, 418);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 20);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Total Amt";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(628, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tp);
+            this.Controls.Add(this.tt);
+            this.Controls.Add(this.bill);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.amt);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.qun);
+            this.Controls.Add(this.tot);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tot;
+        private System.Windows.Forms.TextBox qun;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.TextBox amt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox bill;
+        private System.Windows.Forms.TextBox tt;
+        private System.Windows.Forms.TextBox tp;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ billing2/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace billing2
+{
+    public partial class Form1 : Form
+    {
+        double tax1=0, price1=0,tax2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            wramt();
+            wtot();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            wtot();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tot.Text != "")
+            {
+                listBox1.Items.Add(comboBox1.Text + "\t" + price.Text + "\t" + qun.Text + "\t" + amt.Text + "\t" + comboBox2.Text + "\t" + tot.Text);
+                tax1 += tax2;
+                price1 += int.Parse(amt.Text);
+            }
+            tt.Text = tax1 + "";
+            tp.Text = price1 + "";
+            bill.Text = (tax1 + price1) + "";
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+                price.Text = "50";
+            else if (comboBox1.SelectedIndex == 1)
+                price.Text = "150";
+            else if (comboBox1.SelectedIndex == 2)
+                price.Text = "250";
+            else
+                price.Text = "500";
+            wramt();
+            wtot();
+            
+        }
+
+        public void wramt()
+        {
+            if (qun.Text != "" && price.Text != "")
+                amt.Text = int.Parse(qun.Text) * int.Parse(price.Text) + "";
+            else
+                amt.Text = "";
+        }
+
+        public void wtot()
+        {
+            if (qun.Text != "" && price.Text != "" && comboBox2.Text != "")
+            {
+                double t;
+                if (comboBox2.SelectedIndex == 0)
+                    t = 7;
+                else if (comboBox2.SelectedIndex == 1)
+                    t = 6.5;
+                else if (comboBox2.SelectedIndex == 2)
+                    t = 6;
+                else if (comboBox2.SelectedIndex == 3)
+                    t = 5.5;
+                else
+                    t = 5;
+                tax2 = int.Parse(amt.Text) * t / 100;
+                tot.Text = (int.Parse(amt.Text) + tax2) + "";
+            }
+            else
+                tot.Text = "";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+        }
+    }
+}
